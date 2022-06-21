fcc848fd98da25c01762597c15a9cdd34fa7f453
Vera Ho <vh71886@gmail.com>
2022-06-21 10:33:53 -0400

    Added header, search background images - CSS

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import HeaderContainer from "./header/header_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
-// import SearchContainer from "./bench/search_container";
+import SearchContainer from "./search/search_container";
 
 import { Route } from 'react-router-dom';
 import { AuthRoute } from "../util/route_util";
 
 const App = () => (
   <div>
-    <header>
+    <header className="header">
       <HeaderContainer />
-      {/* <h1>Welcome to Terra l'Hotel - App</h1> */}
     </header>
 
     {/* Need auth and protected routes to prevent these from rendering when logged in */}
+    <Route exact path="/" component={SearchContainer} />
     <AuthRoute path="/signin" component={LoginFormContainer} />
     <AuthRoute path="/register" component={SignupFormContainer} />
     <br></br>
-    {/* <Route exact path="/" component={SearchContainer} /> */}
   </div>
 );
 
