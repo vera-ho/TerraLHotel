604caa26c43b51f9e2ce2506fa2b8f87ed1fd575
Vera Ho <vh71886@gmail.com>
2022-06-24 16:17:15 -0400

    CSS hotel details

--- app/views/api/hotels/show.json.jbuilder
+++ app/views/api/hotels/show.json.jbuilder
@@ -4,10 +4,9 @@ json.hotel do
     json.partial! 'hotel', hotel: @hotel
 end
 
-# this throws error - GET 500 Internal Server Error
 json.rooms do
     rooms = @hotel.hotel_rooms
-    @hotel.hotel_rooms.each do |room|
+    rooms.each do |room|
         json.set! room.id do
             json.partial! '/api/rooms/room', room: room
         end
--- frontend/components/hotel/hotel.jsx
+++ frontend/components/hotel/hotel.jsx
@@ -14,49 +14,58 @@ class Hotel extends React.Component {
 
     render() {
         const hotel = this.props.hotel;
+        let amentities = ["100% non-smoking hotel", "24 hour front desk", 
+            "Central air conditioning", "Free wi-fi", "Lounge", "Luggage Storage", 
+            "Multi-lingual Staff", "Restaurant", "Room service", "Shopping nearby", 
+            "Spa treatments", "Wake-up service"];
+
+        amentities = amentities.map( (amenity, idx) => {
+            return (<li key={idx}>{amenity}</li>)
+        })
 
         if(!hotel) return (<h2 className="loading">Loading...</h2>)
         return (
             <section className="hotel-details-container">
-                {/* <h1>Hotel Listing Details Page</h1> */}
-                <h2 className="hotel-details-name">{hotel.name}</h2>
-                <p>{hotel.address}, {hotel.country}</p>
-
-                <section className="hotel-details-images">
-                </section>
-
-                <div className="hotel-details-misc">
-                </div>
-
-                <div className="hotel-details-description">
-                    <p>Praesent feugiat nisl cursus, bibendum ligula id, dignissim lacus. Sed vel facilisis justo. Aliquam tincidunt lorem eget congue dictum. Nullam non est at nisi vulputate feugiat. Morbi sit amet massa a diam malesuada pulvinar. Phasellus elementum, ante ut pharetra pellentesque, augue odio commodo risus, eu feugiat quam neque eget elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut finibus dignissim ex, nec egestas lacus tempus sed. Pellentesque tincidunt vulputate fermentum. Donec convallis ligula condimentum massa accumsan, quis bibendum quam condimentum. Ut dictum enim vel libero venenatis feugiat.</p>
-
-                    <p>{hotel.rooms} rooms</p>
-                    <p>Style: Modern</p>
-                    <p>Atmosphere: Quiet</p>
-                </div>
-
-                <div className="hotel-details-map">
-                </div>
-
-                <div className="hotel-details-amenities">
-                    <h3>Hotel Amenities and Services</h3>
-                    <ul>
-                        <li>
-                            Insert list here
-                        </li>
-                    </ul>
-                </div>
-
-                <RoomsIndex rooms={this.props.rooms} />
-                {/* <aside className="rooms-index-container">
-                    <h3>Rooms</h3>
-                    <p>Best price guaranteed.</p>
-                    <ul>
-                        {rooms}
-                    </ul>
-                </aside> */}
+                <header>
+                    <p className="hotel-details-location">{hotel.country + " / " + hotel.city}</p>
+                    <h2 className="hotel-details-name">{hotel.name}</h2>
+                    <p>{hotel.address}, {hotel.country}</p>
+                </header>
                 
+                <main className="hotel-details-content">
+                    <div className="hotel-content-left">
+                        <section className="hotel-details-images">
+                        </section>
+
+                        <section className="hotel-details-misc">
+                        </section>
+
+                        <div className="hotel-details-description">
+                            <p>Praesent feugiat nisl cursus, bibendum ligula id, dignissim lacus. Sed vel facilisis justo. Aliquam tincidunt lorem eget congue dictum. Nullam non est at nisi vulputate feugiat. Morbi sit amet massa a diam malesuada pulvinar. Phasellus elementum, ante ut pharetra pellentesque, augue odio commodo risus, eu feugiat quam neque eget elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut finibus dignissim ex, nec egestas lacus tempus sed. Pellentesque tincidunt vulputate fermentum. Donec convallis ligula condimentum massa accumsan, quis bibendum quam condimentum. Ut dictum enim vel libero venenatis feugiat.</p>
+                        </div>
+
+                        <div className="hotel-details-bullets">
+                            <p>{hotel.rooms} rooms</p>
+                            <p>Style: Modern</p>
+                            <p>Atmosphere: Quiet</p>
+                        </div>
+
+                        <div className="hotel-details-map">
+                        </div>
+
+                        <div className="hotel-details-reviews">
+                        </div>
+
+                        <div className="hotel-details-amenities">
+                            <h3>Hotel Amenities and Services</h3>
+                            <ul className="hotel-amentities">
+                                {amentities}
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <RoomsIndex rooms={this.props.rooms} />
+                </main>
             </section>
         )
     }
