6bc18ebfade719dc6acc5b3ee6d04061d9aef0a2
Vera Ho <vh71886@gmail.com>
2022-06-25 15:14:29 -0400

    First run of reservations controller

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,4 +1,50 @@
 class Api::ReservationsController < ApplicationController
 
-    
+    # User can only view their own reservations when logged in (=> current_user)
+    before_action :require_logged_in    # only: [:show, :create, :update, :destroy]
+
+    def index
+        @reservations = Reservation.find_by(customer_id: params[:customer_id])
+        # @reservations = current_user.reservations
+    end
+
+    # NEST INDEX/SHOW UNDER USER???
+
+    def show
+        @reservation = Reservation.find_by(id: params[:id])
+    end
+
+    def create
+        @reservation = Reservation.new(reservation_params)
+        @reservation.customer_id = current_user.id
+        # @reservation = current_user.reservations.new(reservation_params)
+        if @reservation.save
+            render :show
+        else
+            render json: ['Room could not be reserved. Please check your reservation information and try again.'], status: 422
+        end
+    end
+
+    def update
+        @reservation = current_user.reservations.find_by(id: params[:id])
+        if @reservation && @reservation.update(reservation_params)
+            render :show
+        else
+            render json: ['Reservation coujld not be updated. Please check your reservation information and try again'], status: 422
+        end
+    end
+
+    def destroy
+        @reservation = current_user.reservations.find_by(id: params[:id])
+        if @reservation && @reservation.destroy
+            render :index
+        else
+            render json: ['Something went wrong. Reservation could not be cancelled.'], status 422
+        end
+    end
+
+    private
+    def reservation_params
+        params.require(:reservation).permit(:hotel_id, :customer_id, :room_id, :checkin, :checkout, :status)
+    end
 end
