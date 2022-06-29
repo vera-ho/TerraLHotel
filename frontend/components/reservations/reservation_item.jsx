d952e5e8d06239c82c55de4a434012982e1fbb76
Vera Ho <vh71886@gmail.com>
2022-06-29 09:49:08 -0400

    Modified reservation date format

--- frontend/components/reservations/reservation_item.jsx
+++ frontend/components/reservations/reservation_item.jsx
@@ -6,26 +6,35 @@ const ReservationItem = props => {
         props.requestHotel(props.reservation.hotelId);
     }, [])
 
-    const hotel = props.hotels[props.reservation.hotelId];
-    console.log("Hotel: " + hotel);
 
     const handleCancelClick = e => {
         e.preventDefault();
         props.cancelReservation(props.reservation.id);
     }
 
+    const hotel = props.hotels[props.reservation.hotelId];
+
+    const locale =  navigator.language || navigator.browserLanguage || (navigator.languages || ["en"])[0];
+    const checkin = new Date(props.reservation.checkin);
+    const checkout = new Date(props.reservation.checkout);
+    const options = {
+        month: "long",
+        year: "numeric",
+        day: "2-digit"
+    }
+ 
     if(!hotel) return <p>Loading...</p>
     return (
         <li>
             <div>
                 <p>Name: {props.user.fname + " " + props.user.lname}</p>
                 <p>Hotel: {hotel.name}</p>
-                <p>Check-in: {props.reservation.checkin}</p>
-                <p>Check-out: {props.reservation.checkout}</p>
+                <p>Check-in: {checkin.toLocaleDateString(locale, options)}</p>
+                <p>Check-out: {checkout.toLocaleDateString(locale, options)}</p>
                 <p>Status: {props.reservation.status}</p>
             </div>
             <div>
-                <button >Edit Reservation</button>
+                <button to={`/reservation/edit/${props.reservation.id}`}>Edit Reservation</button>
                 <button onClick={handleCancelClick}>Cancel Reservation</button>
             </div>
         </li>
