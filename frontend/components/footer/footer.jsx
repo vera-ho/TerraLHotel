4d96fe9a1d6898e7560511c5919bf61c8a96f826
Vera Ho <vh71886@gmail.com>
2022-06-30 21:19:48 -0400

    Styling and adding to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,13 +9,26 @@ const FooterIndex = props => {
     return (
         <div className="footer-index-container">
             <section>
-                <label>Technologies Used</label>
+                <h1>Technologies Used</h1>
                 <ul>
                     <li>React and Redux</li>
                     <li>Ruby on Rails</li>
                     <li>Google Maps API</li>
                     <li>Javascript</li>
                 </ul>
+            </section>
+                
+            <section>
+                <h1>Developer</h1>
+                <ul>
+                    <li>GitHub</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </section>
+            
+            <section>
+                <p>Terra l'Hotel is a clone of Tablet Hotels</p>
+                <p>Images belong to Tablet Hotels</p>
             </section>
         </div>
     )
