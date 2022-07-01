ec583c1d33d165cf83af3027c58c67a0a88fe187
Vera Ho <vh71886@gmail.com>
2022-06-30 20:31:45 -0400

    Add image links to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -107,72 +107,108 @@ Hotel.create({
 Room.destroy_all
 
 # 1, 2
-Room.create({ room_type: "Deluxe Room", beds: "1 double/full bed" , occupancy: 2, size: "215 ft²/20 m²", building_id: 1 })
-Room.create({ room_type: "Standard Room", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "215 ft²/20 m²", building_id: 1 })
+Room.create({ room_type: "Deluxe Room", beds: "1 double/full bed" , occupancy: 2, size: "215 ft²/20 m²", building_id: 1, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
+Room.create({ room_type: "Standard Room", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "215 ft²/20 m²", building_id: 1,
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
 
 # 3
-Room.create({ room_type: "Prestige Suite with Private Pool", beds: "1 King Bed", occupancy: 2, size: "807 ft²/75 m²", building_id: 2 })
+Room.create({ room_type: "Prestige Suite with Private Pool", beds: "1 King Bed", occupancy: 2, size: "807 ft²/75 m²", building_id: 2,
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
 
 # 4, 5, 6
-Room.create({ room_type: "Muse", beds: "1 Queen Bed", occupancy: 2, size: "215 ft²/20 m²", building_id: 3 })
-Room.create({ room_type: "Goddess Suite", beds: "2 double beds", occupancy: 4, size: "269 ft²/25 m²", building_id: 3 })
-Room.create({ room_type: "Suite with Terrace", beds: "1 Double/Full Bed" , occupancy: 2, size: "430 ft²/40 m²", building_id: 3 })
+Room.create({ room_type: "Muse", beds: "1 Queen Bed", occupancy: 2, size: "215 ft²/20 m²", building_id: 3,
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128524390502/room005_th.jpeg"})
+Room.create({ room_type: "Goddess Suite", beds: "2 double beds", occupancy: 4, size: "269 ft²/25 m²", building_id: 3, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
+Room.create({ room_type: "Suite with Terrace", beds: "1 Double/Full Bed" , occupancy: 2, size: "430 ft²/40 m²", building_id: 3, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
 
 # 7, 8
-Room.create({ room_type: "Glass Cottage", beds: "1 Queen Bed", occupancy: 2, size: "139 ft²/13 m²", building_id: 4 })
-Room.create({ room_type: "Deluxe Suite Grove View", beds: "1 King Bed", occupancy: 2, size: "678 ft²/63 m²", building_id: 4 })
+Room.create({ room_type: "Glass Cottage", beds: "1 Queen Bed", occupancy: 2, size: "139 ft²/13 m²", building_id: 4, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
+Room.create({ room_type: "Deluxe Suite Grove View", beds: "1 King Bed", occupancy: 2, size: "678 ft²/63 m²", building_id: 4, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
 
 # 9, 10, 11
-Room.create({ room_type: "Premium", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "161 ft²/15 m²", building_id: 5 })
-Room.create({ room_type: "Superior", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "193 ft²/18 m²", building_id: 5 })
-Room.create({ room_type: "Superior Triple", beds: "3 Single Beds or 1 Double & 1 Single Bed", occupancy: 3, size: "258 ft²/24 m²", building_id: 5 })
+Room.create({ room_type: "Premium", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "161 ft²/15 m²", building_id: 5, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
+Room.create({ room_type: "Superior", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "193 ft²/18 m²", building_id: 5, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128524390502/room005_th.jpeg"})
+Room.create({ room_type: "Superior Triple", beds: "3 Single Beds or 1 Double & 1 Single Bed", occupancy: 3, size: "258 ft²/24 m²", building_id: 5, 
+     img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
 
 # 12, 13
-Room.create({ room_type: "Maisonette", beds: "2 Single Beds", occupancy: 4, size: "462 ft²/43 m²", building_id: 6})
-Room.create({ room_type: "Triple Room", beds: "3 Single Beds", occupancy: 4, size: "312 ft²/29 m²", building_id: 6})
+Room.create({ room_type: "Maisonette", beds: "2 Single Beds", occupancy: 4, size: "462 ft²/43 m²", building_id: 6, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
+Room.create({ room_type: "Triple Room", beds: "3 Single Beds", occupancy: 4, size: "312 ft²/29 m²", building_id: 6, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
 
 # 14, 15, 16
-Room.create({ room_type: "Athena Suite", beds: "1 King Bed & 2 Double Beds", occupancy: 6, size: "1345 ft²/124 m²", building_id: 7})
-Room.create({ room_type: "Hoyt House Deluxe Suite", beds: "1 King Bed", occupancy: 2, size: "635 ft²/58 m²", building_id: 7})
-Room.create({ room_type: "Hoyt House Grand Suite", beds: "1 King Bed", occupancy: 2, size: "1228 ft²/114 m²", building_id: 7})
+Room.create({ room_type: "Athena Suite", beds: "1 King Bed & 2 Double Beds", occupancy: 6, size: "1345 ft²/124 m²", building_id: 7, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
+Room.create({ room_type: "Hoyt House Deluxe Suite", beds: "1 King Bed", occupancy: 2, size: "635 ft²/58 m²", building_id: 7, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
+Room.create({ room_type: "Hoyt House Grand Suite", beds: "1 King Bed", occupancy: 2, size: "1228 ft²/114 m²", building_id: 7, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128524390502/room005_th.jpeg"})
 
 # 17, 18
-Room.create({ room_type: "Club Room", beds: "1 King Bed", occupancy: 2, size: "365 ft²/34 m²", building_id: 8})
-Room.create({ room_type: "Club Suite", beds: "1 King Bed", occupancy: 2, size: "731 ft²/68 m²", building_id: 8})
+Room.create({ room_type: "Club Room", beds: "1 King Bed", occupancy: 2, size: "365 ft²/34 m²", building_id: 8, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
+Room.create({ room_type: "Club Suite", beds: "1 King Bed", occupancy: 2, size: "731 ft²/68 m²", building_id: 8, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128524390502/room005_th.jpeg"})
 
 # 19, 20
-Room.create({ room_type: "Volkshaus", beds: "1 King Bed", occupancy: 3, size: "355 ft²/33 m²", building_id: 9})
-Room.create({ room_type: "Corvatsch Room", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "269 ft²/25 m²", building_id: 9})
+Room.create({ room_type: "Volkshaus", beds: "1 King Bed", occupancy: 3, size: "355 ft²/33 m²", building_id: 9, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
+Room.create({ room_type: "Corvatsch Room", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "269 ft²/25 m²", building_id: 9, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
 
 # 21, 22
-Room.create({ room_type: "Marquis Suite", beds: "1 King Bed", occupancy: 2, size: "861 ft²/79 m²", building_id: 10})
-Room.create({ room_type: "Eiffel Suite", beds: "1 King Bed", occupancy: 2, size: "650 ft²/60 m²", building_id: 10})
+Room.create({ room_type: "Marquis Suite", beds: "1 King Bed", occupancy: 2, size: "861 ft²/79 m²", building_id: 10, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
+Room.create({ room_type: "Eiffel Suite", beds: "1 King Bed", occupancy: 2, size: "650 ft²/60 m²", building_id: 10, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
 
 # 23, 24, 25, 26
-Room.create({ room_type: "Arno River View", beds: "1 Queen Bed", occupancy: 2, size: "484 ft²/44 m²", building_id: 11})
-Room.create({ room_type: "Superior Junior Suite", beds: "1 King Bed", occupancy: 4, size: "592 ft²/55 m²", building_id: 11})
-Room.create({ room_type: "Grand Premium with View", beds: "1 King Bed", occupancy: 3, size: "355 ft²/33 m²", building_id: 11})
-Room.create({ room_type: "Two Bedroom VIP Suite", beds: "1 Queen Bed", occupancy: 2, size: "914 ft²/85 m².", building_id: 11})
+Room.create({ room_type: "Arno River View", beds: "1 Queen Bed", occupancy: 2, size: "484 ft²/44 m²", building_id: 11, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
+Room.create({ room_type: "Superior Junior Suite", beds: "1 King Bed", occupancy: 4, size: "592 ft²/55 m²", building_id: 11, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
+Room.create({ room_type: "Grand Premium with View", beds: "1 King Bed", occupancy: 3, size: "355 ft²/33 m²", building_id: 11, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128524390502/room005_th.jpeg"})
+Room.create({ room_type: "Two Bedroom VIP Suite", beds: "1 Queen Bed", occupancy: 2, size: "914 ft²/85 m².", building_id: 11, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
 
 # 27, 28, 29
-Room.create({ room_type: "Standard Wide + Balcony", beds: "1 Double/Full Bed", occupancy: 2, size: "247 ft²/23 m²", building_id: 12})
-Room.create({ room_type: "Deluxe Palace Garden View", beds: "1 King Bed", occupancy: 2, size: "764 ft²/70 m²", building_id: 12})
-Room.create({ room_type: "Susuki King", beds: "1 King Bed", occupancy: 2, size: "646 ft²/60 m²", building_id: 12})
+Room.create({ room_type: "Standard Wide + Balcony", beds: "1 Double/Full Bed", occupancy: 2, size: "247 ft²/23 m²", building_id: 12, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
+Room.create({ room_type: "Deluxe Palace Garden View", beds: "1 King Bed", occupancy: 2, size: "764 ft²/70 m²", building_id: 12, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
+Room.create({ room_type: "Susuki King", beds: "1 King Bed", occupancy: 2, size: "646 ft²/60 m²", building_id: 12, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128524390502/room005_th.jpeg"})
 
 # 30
-Room.create({ room_type: "Alcoba", beds: "1 Queen Bed", occupancy: 2, size: "204 ft²/19 m²", building_id: 13})
+Room.create({ room_type: "Alcoba", beds: "1 Queen Bed", occupancy: 2, size: "204 ft²/19 m²", building_id: 13, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127811358770/room002_th.jpeg"})
 
 # 31
-Room.create({ room_type: "Aposento", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "322 ft²/30 m².", building_id: 14})
+Room.create({ room_type: "Aposento", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "322 ft²/30 m².", building_id: 14, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
 
 # 32, 33, 34
-Room.create({ room_type: "Dawn Room", beds: "2 Double Beds", occupancy: 3, size: "430 ft²/39 m²", building_id: 15})
-Room.create({ room_type: "Bungalow", beds: "2 Single Beds", occupancy: 2, size: "484 ft²/45 m²", building_id: 15})
-Room.create({ room_type: "Park Family Suite", beds: "1 Double & 2 Single Beds", occupancy: 5, size: "645 ft²/60 m²", building_id: 15})
+Room.create({ room_type: "Dawn Room", beds: "2 Double Beds", occupancy: 3, size: "430 ft²/39 m²", building_id: 15, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
+Room.create({ room_type: "Bungalow", beds: "2 Single Beds", occupancy: 2, size: "484 ft²/45 m²", building_id: 15, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128306278562/room004_th.jpeg"})
+Room.create({ room_type: "Park Family Suite", beds: "1 Double & 2 Single Beds", occupancy: 5, size: "645 ft²/60 m²", building_id: 15, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186128524390502/room005_th.jpeg"})
 
 #35, 36
-Room.create({ room_type: "One Bedroom Tent", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "42 ft²/3 m²", building_id: 16})
-Room.create({ room_type: "Backyward Winetank Suite", beds: "1 King Bed", occupancy: 3, size: "322 ft²/30 m²", building_id: 16})
+Room.create({ room_type: "One Bedroom Tent", beds: "2 Single Beds or 1 King Bed", occupancy: 2, size: "42 ft²/3 m²", building_id: 16, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room005_th.jpeg"})
+Room.create({ room_type: "Backyward Winetank Suite", beds: "1 King Bed", occupancy: 3, size: "322 ft²/30 m²", building_id: 16, 
+    img1: "https://cdn.discordapp.com/attachments/862515957842706475/992186127584870481/room001_th.jpeg"})
 
 ## ************* RESERVATION SEEDS ************* ##
 Reservation.destroy_all
@@ -188,4 +224,3 @@ Reservation.create({ hotel_id: 12, room_id: 29, customer_id: 2, checkin: "Januar
 Reservation.create({ hotel_id: 13, room_id: 30, customer_id: 2, checkin: "January 19, 2023", checkout: "February 14, 2023", status: "booked" })
 Reservation.create({ hotel_id: 15, room_id: 33, customer_id: 1, checkin: "January 12, 2023", checkout: "January 19, 2023", status: "booked" })
 Reservation.create({ hotel_id: 16, room_id: 35, customer_id: 2, checkin: "May 5, 2023", checkout: "December 12, 2023", status: "updated" })
-
