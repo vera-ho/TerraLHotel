65768b456b7a68468b4f9017121e509cd815e3f5
Vera Ho <vh71886@gmail.com>
2022-07-20 19:06:32 -0400

    Add date information to review index. Delete review item file

--- frontend/components/reviews/reviews_index.jsx
+++ frontend/components/reviews/reviews_index.jsx
@@ -1,17 +1,14 @@
 import React, { useEffect } from "react";
 
 const ReviewsIndex = props => {
-    // debugger
     const {hotel, reviews} = props;
-
-    // const options = { month: 'long'};
-    // console.log(new Intl.DateTimeFormat('en-US', options).format(Xmas95));
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getMonth
+    const options = { month: 'long'};
 
     const reviewItems = Object.values(reviews).map( (review, idx) => {
-        // let month = review.createdAt.getMonth();
-        // let year = review.createdAt.getFullYear();
-        debugger
+        let date = new Date(review.createdAt);
+        let month = new Intl.DateTimeFormat('en-US', options).format(date);
+        let year = date.getFullYear();
+
         return (
             <li key={idx} className="review-item-container">
                 <h2>What I liked:</h2>
@@ -20,12 +17,11 @@ const ReviewsIndex = props => {
                 <h2>What the hotel could do better:</h2>
                 {/* <p>{review.cons}</p> */}
                 <div className="review-item-reviewer-info">
-                    {/* <p>{review.reviewer.fname}</p> */}
-                    {/* <p>{month + " " + year}</p> */}
+                    <p>{review.reviewer.fname}</p>
+                    <p>{month + " " + year}</p>
                 </div>
             </li>
         )
-        // return (<ReviewItem review={review} props={props} key={idx} />)
     })
 
     return (
