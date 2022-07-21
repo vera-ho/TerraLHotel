45bbd057b8347bf3ce20a4667226eae2fc0a0fe6
Vera Ho <vh71886@gmail.com>
2022-07-21 19:35:20 -0400

    Update reducers for modal functionality

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,3 +1,4 @@
+import { CLOSE_MODAL } from "../actions/modal_actions";
 import { 
     RECEIVE_SESSION_ERRORS,
     RECEIVE_CURRENT_USER,
@@ -13,6 +14,8 @@ const sessionErrorsReducer = (state = {}, action) => {
         case RECEIVE_CURRENT_USER:
             return [];
         case CLEAR_ERRORS:
+            return [];
+        case CLOSE_MODAL: 
             return [];
         default:
             return state;
