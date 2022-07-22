38a048980d5c19dee8762c8e204a3af6e638bd81
Vera Ho <vh71886@gmail.com>
2022-07-21 20:24:43 -0400

    Remove title and rating columns from reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_231058) do
+ActiveRecord::Schema.define(version: 2022_07_22_002309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,6 @@ ActiveRecord::Schema.define(version: 2022_07_20_231058) do
   create_table "reviews", force: :cascade do |t|
     t.integer "reviewed_hotel_id", null: false
     t.integer "reviewer_id", null: false
-    t.integer "rating", null: false
-    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "pros", null: false
