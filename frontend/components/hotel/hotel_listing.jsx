8a442616982393b8f61c90ede7c0eba22b1fb548
Vera Ho <vh71886@gmail.com>
2022-07-25 22:15:12 -0400

    Update hotel listing CSS for media size

--- frontend/components/hotel/hotel_listing.jsx
+++ frontend/components/hotel/hotel_listing.jsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
+import { TiHeartFullOutline } from 'react-icons/ti'
 
 const HotelListing = props => {
-    const [name, setName] = useState(props.hotel.name);
-    const [country, useCountry] = useState(props.hotel.country);
-    const [city, setCity] = useState(props.hotel.city);
-    const [rooms, setRooms] = useState(props.hotel.rooms);
-
-    // const urlName = name.replaceAll(" ", "-")
+    const { hotel } = props; 
     
     return (
         <section className='hotel-listing-description'>
-            <Link to={`/hotels/${props.hotel.id}`} >
-                <img src={props.hotel.img1}
+            <Link to={`/hotels/${hotel.id}`} >
+                <img src={hotel.img1}
                     width='160px'
                     height='160px'
                 />
@@ -20,19 +16,15 @@ const HotelListing = props => {
                 <div id='hotel-card-2'></div>
             </Link>
             <li className='hotel-listing-item'>
-                <Link to={`/hotels/${props.hotel.id}`} 
+                <Link to={`/hotels/${hotel.id}`} 
                     className='hotel-listing-name'>
-                        {name}
+                        {hotel.name}
                 </Link>
-                <p>{city}, {country}</p>
-                <p>Total Rooms: {rooms}</p>
+                <p>{hotel.city}, {hotel.country}</p>
+                <p>Total Rooms: {hotel.rooms}</p>
                 <br></br>
             </li>
-            <img src='https://cdn.discordapp.com/attachments/862515957842706475/989278866285678642/unknown.png'
-                width='40px'
-                height='40px'
-            />
-            {/* <FontAwesomeIcon icon="fa-solid fa-heart" /> */}
+            <TiHeartFullOutline className='hotel-favorite-icon' />
         </section>
 
     )
