d661136b34748c68a4e48af42cfa8097159ec52a
Vera Ho <vh71886@gmail.com>
2022-07-25 20:48:22 -0400

    Refactor footer CSS

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,16 +3,12 @@ import { BsGithub, BsLinkedin, BsPersonCircle } from 'react-icons/bs';
 import { SiAngellist } from 'react-icons/si';
 
 const FooterIndex = (props) => {
-    // list technologies
-    // developer section with personal links
-    // link to original site
-    // Section to do copyright/for educational purposes/etc.
     
     return (
         <div className="footer-index-container">
-            <section>
+            <section className="footer-technologies-used">
                 <h1>Technologies Used</h1>
-                <ul>
+                <ul className="footer-technologies-list">
                     <li>React and Redux</li>
                     <li>Ruby on Rails</li>
                     <li>Google Maps API</li>
@@ -20,38 +16,36 @@ const FooterIndex = (props) => {
                 </ul>
             </section>
                 
-            <section>
-                <div>
+            <section className="footer-developer-info-container">
+                <div className="footer-developer-info-content">
                     <h1>Developer</h1>
-                    <ul>
-                        <li>
-                            <a href="https://github.com/vh71886/">
+                    <ul className="footer-developer-info-list">
+                        <li className="footer-developer-info-list-item">
+                            <a href="https://github.com/vh71886/" target="_blank" rel="noopener noreferrer">
                                 <BsGithub  className="developer-icon" />
                             </a>
                         </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/veraho/">
+                        <li className="footer-developer-info-list-item">
+                            <a href="https://www.linkedin.com/in/veraho/" target="_blank" rel="noopener noreferrer">
                                 <BsLinkedin className="developer-icon" />
                             </a>
                         </li>
-                        <li>
-                            <a href="https://angel.co/u/vera-ho-1">
+                        <li className="footer-developer-info-list-item">
+                            <a href="https://angel.co/u/vera-ho-1" target="_blank" rel="noopener noreferrer">
                                 <SiAngellist className="developer-icon" />
                             </a>
                         </li>
-                        <li>
-                            <a href="#">
+                        <li className="footer-developer-info-list-item">
+                            <a href="#" target="_blank" rel="noopener noreferrer">
                                 <BsPersonCircle className="developer-icon" />
                             </a>
                         </li>
                     </ul>
                 </div>
-                <div>
-                    {/* Project Repository */}
-                </div>
+                <div className="footer-project-repo"></div>
             </section>
             
-            <section>
+            <section className="footer-about">
                 <h1>About</h1>
                 <p>Terra l'Hotel is a clone of Tablet Hotels</p>
                 <p>Images belong to Tablet Hotels</p>
