d708045146c99bb4f5efab1603a30bcf1b01d081
Vera Ho <vh71886@gmail.com>
2022-07-28 16:49:10 -0400

    Cleanup user reviews index

--- frontend/components/reviews/user_reviews_index.jsx
+++ frontend/components/reviews/user_reviews_index.jsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
-// import Modal from 'react-modal';
+import React, { useEffect } from "react";
 import UserReviewItem from "./review_item";
-import ReviewForm from "./review_form";
-// import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { requestAllHotels } from "../../actions/hotel_actions";
 import { getUser } from "../../actions/session_actions";
@@ -11,17 +8,12 @@ import { editReview, deleteReview } from "../../actions/review_actions";
 const UserReviewsIndex = props => {
     const { requestAllHotels, getUser, deleteReview } = props;
     const { user, reviews, hotels} = props || {};
-    // const [showModal, setShowModal] = useState(false);
     const numReviews = Object.values(reviews).length;
 
     useEffect( () => {
         getUser(user.id);
         requestAllHotels();
     }, [])
-
-    // const handleEditReview = () => {
-    //     setShowModal(true);
-    // }
 
     const reviewsList = Object.values(reviews).map( (review, idx) => {
         const hotel = hotels[review.reviewedHotelId] || {};
@@ -35,59 +27,6 @@ const UserReviewsIndex = props => {
                 editReview={editReview}
                 key={idx}
             />
-            // <li key={idx} className="user-review-item-container">
-            //     <div>
-            //         <div className="user-review-item-content">
-            //             <p>
-            //                 <Link to={`/hotels/${hotel.id}`} className="user-review-item-hotel">
-            //                     {hotel.name}
-            //                 </Link>
-            //             </p>
-
-            //             <h2>What you liked:</h2>
-            //             <p>{review.pros}</p>
-
-            //             {review.cons.length > 0 ? (
-            //                 <>
-            //                     <h2>What the hotel could have done better:</h2>
-            //                     <p>{review.cons}</p>
-            //                 </>
-            //             ) : (
-            //                 <></>
-            //             )}
- 
-            //         </div>
-            //         <div className="user-review-item-actions">
-            //             <Link to={{}} onClick={handleEditReview}
-            //                 className='btn'>Edit Review</Link>
-            //             <Modal
-            //                 className="edit-review-form-modal"
-            //                 isOpen={showModal}
-            //                 shouldCloseOnOverlayClick={true}
-            //                 onRequestClose={() => setShowModal(false) }
-            //                 ariaHideApp={false}
-            //                 style={{
-            //                     overlay: {
-            //                     backgroundColor: 'rgba(0, 0, 0, 0.5)'
-            //                     }
-            //                 }}
-            //             >
-            //                 <ReviewForm
-            //                     closeModal={ () => setShowModal(false) }
-            //                     hotelId={review.reviewedHotelId}
-            //                     reviewerId={review.reviewerId}
-            //                     title={`Update your thoughts for ${hotel.name}!`}
-            //                     submitForm={ review => dispatch(editReview(review))}
-            //                     formType="edit"
-            //                     review={review}
-            //                 />
-            //             </Modal>
-                        
-            //             <Link to={{}} onClick={() => deleteReview(review.id)} 
-            //                 className='btn'>Delete Review</Link>                       
-            //         </div>
-            //     </div>
-            // </li>
         )
     })
 
