394151f5307a409c6a48c309f9fc5633661bf04a
Vera Ho <vh71886@gmail.com>
2022-07-28 17:17:28 -0400

    Remove favorites heart icon until favorites are implemented

--- frontend/components/hotel/hotel_listing.jsx
+++ frontend/components/hotel/hotel_listing.jsx
@@ -24,7 +24,7 @@ const HotelListing = props => {
                 <p>Total Rooms: {hotel.rooms}</p>
                 <br></br>
             </li>
-            <TiHeartFullOutline className='hotel-favorite-icon' />
+            {/* <TiHeartFullOutline className='hotel-favorite-icon' /> */}
         </section>
     )
 }
