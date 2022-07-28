b5673c0292235da92e65f4588de298d2f31b5ed5
Vera Ho <vh71886@gmail.com>
2022-07-28 11:33:46 -0400

    Added basic call to Impala API

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,36 @@
 Rails.application.routes.draw do
-  get 'search/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
 
-#           Prefix Verb   URI Pattern                          Controller#Action
-#             root GET    /                                    static_pages#root
-#        api_users POST   /api/users(.:format)                 api/users#create {:default=>{:format=>:json}}
-#         api_user GET    /api/users/:id(.:format)             api/users#show {:default=>{:format=>:json}}
-#                  PATCH  /api/users/:id(.:format)             api/users#update {:default=>{:format=>:json}}
-#                  PUT    /api/users/:id(.:format)             api/users#update {:default=>{:format=>:json}}
+  # Route for API calls
+  # get 'search/index'
+  get '/search' => 'search#index', :as => 'search'
+  
 
-#       api_hotels GET    /api/hotels(.:format)                api/hotels#index {:default=>{:format=>:json}}
-#        api_hotel GET    /api/hotels/:id(.:format)            api/hotels#show {:default=>{:format=>:json}}
-
-# api_reservations GET    /api/reservations(.:format)          api/reservations#index {:default=>{:format=>:json}}
-#                  POST   /api/reservations(.:format)          api/reservations#create {:default=>{:format=>:json}}
-#  api_reservation GET    /api/reservations/:id(.:format)      api/reservations#show {:default=>{:format=>:json}}
-#                  PATCH  /api/reservations/:id(.:format)      api/reservations#update {:default=>{:format=>:json}}
-#                  PUT    /api/reservations/:id(.:format)      api/reservations#update {:default=>{:format=>:json}}
-#                  DELETE /api/reservations/:id(.:format)      api/reservations#destroy {:default=>{:format=>:json}}
-
-#      api_session DELETE /api/session(.:format)               api/sessions#destroy {:default=>{:format=>:json}}
-#                  POST   /api/session(.:format)               api/sessions#create {:default=>{:format=>:json}}
+#                 Prefix Verb   URI Pattern                                     Controller#Action
+#                 search GET    /search(.:format)                               search#index
+#                   root GET    /                                               static_pages#root
+#              api_users POST   /api/users(.:format)                            api/users#create {:default=>{:format=>:json}}
+#               api_user GET    /api/users/:id(.:format)                        api/users#show {:default=>{:format=>:json}}
+#                        PATCH  /api/users/:id(.:format)                        api/users#update {:default=>{:format=>:json}}
+#                        PUT    /api/users/:id(.:format)                        api/users#update {:default=>{:format=>:json}}
+#             api_hotels GET    /api/hotels(.:format)                           api/hotels#index {:default=>{:format=>:json}}
+#              api_hotel GET    /api/hotels/:id(.:format)                       api/hotels#show {:default=>{:format=>:json}}
+#              api_rooms GET    /api/rooms(.:format)                            api/rooms#index {:default=>{:format=>:json}}
+#       api_reservations GET    /api/reservations(.:format)                     api/reservations#index {:default=>{:format=>:json}}
+#                        POST   /api/reservations(.:format)                     api/reservations#create {:default=>{:format=>:json}}
+#        api_reservation GET    /api/reservations/:id(.:format)                 api/reservations#show {:default=>{:format=>:json}}
+#                        PATCH  /api/reservations/:id(.:format)                 api/reservations#update {:default=>{:format=>:json}}
+#                        PUT    /api/reservations/:id(.:format)                 api/reservations#update {:default=>{:format=>:json}}
+#                        DELETE /api/reservations/:id(.:format)                 api/reservations#destroy {:default=>{:format=>:json}}
+#            api_reviews GET    /api/reviews(.:format)                          api/reviews#index {:default=>{:format=>:json}}
+#                        POST   /api/reviews(.:format)                          api/reviews#create {:default=>{:format=>:json}}
+#             api_review GET    /api/reviews/:id(.:format)                      api/reviews#show {:default=>{:format=>:json}}
+#                        PATCH  /api/reviews/:id(.:format)                      api/reviews#update {:default=>{:format=>:json}}
+#                        PUT    /api/reviews/:id(.:format)                      api/reviews#update {:default=>{:format=>:json}}
+#                        DELETE /api/reviews/:id(.:format)                      api/reviews#destroy {:default=>{:format=>:json}}
+#            api_session DELETE /api/session(.:format)                          api/sessions#destroy {:default=>{:format=>:json}}
+#                        POST   /api/session(.:format)                          api/sessions#create {:default=>{:format=>:json}}
 
   namespace :api, default: { format: :json } do
     resources :users, only: [:show, :create, :update]
