2259f4caf911d4d99b83282bb818d21aea5905b7
Vera Ho <vh71886@gmail.com>
2022-07-28 23:40:58 -0400

    Add favorite model associations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,9 @@
 class Favorite < ApplicationRecord
+    belongs_to :user,
+        foreign_key: :favoriter_id,
+        class_name: :User
+
+    belongs_to :hotel,
+        foreign_key: :favorited_id,
+        class_name: :Hotel
 end
