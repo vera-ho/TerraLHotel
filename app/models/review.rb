fa9f3c5663d5f3ecaace4462202aa37cc61376ba
Vera Ho <vh71886@gmail.com>
2022-07-28 23:48:47 -0400

    Annotated models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id                :bigint           not null, primary key
+#  reviewed_hotel_id :integer          not null
+#  reviewer_id       :integer          not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  pros              :text             not null
+#  cons              :text             default("")
+#
 class Review < ApplicationRecord
     validates :reviewed_hotel_id, :reviewer_id, :pros, presence: true
 
