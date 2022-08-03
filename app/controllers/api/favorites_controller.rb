ec94d4f9bc776a154d00f9f8c350379f4a87b590
Vera Ho <vh71886@gmail.com>
2022-08-03 13:19:19 -0400

    Add create and delete to favorites controller

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,2 +1,27 @@
 class Api::FavoritesController < ApplicationController
+    # before_action :require_logged_in
+
+    def create
+        @favorite = Favorite.new(favorite_params)
+
+        if @favorite.save
+            render @favorite
+        else
+            render json: @favorite.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @favorite = current_user.favorites.find_by(id: params[:id])
+        if @favorite && @favorite.delete
+            render json: @favorite.id
+        else 
+            render json: @favorite.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def favorite_params
+        params.require(:favorite).permit(:favoriter_id, :favorited_id)
+    end
 end
