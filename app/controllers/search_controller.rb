7f75add2a5e50cee84f482619c8ba1c9da9bc4d0
Vera Ho <vh71886@gmail.com>
2022-08-09 19:50:36 -0400

    Move code back to hotelbeds.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,7 @@
 class SearchController < ApplicationController
     def index
-        # if params['location']
-        #     # @response = Impala::Search.by_location(params['location'])
-        #     @response = HotelBeds::Search.by_location(params['location'])
-        # end
-
-        url = 'https://api.test.hotelbeds.com/hotel-api/1.0/status'
-        xsignature = (ENV['API_KEY'] + ENV['SECRET'] + DateTime.now.strftime("%Q")).hash
-        xsignature = xsignature.to_s
-        
-        @response = HTTParty.get(
-            url,
-            headers: {
-                'api-key': ENV['API_KEY'],
-                'x-signature': xsignature,
-                # "Content-Type": "application/json",
-            }
-        )
+        if params['location']
+            @response = Impala::Search.by_location(params['location'])
+        end
     end
 end
--- app/services/hotelbeds.rb
+++ app/services/hotelbeds.rb
@@ -1,22 +1,27 @@
 module HotelBeds
     class Search
-      def self.by_location(location)
-        url = 'https://api.test.hotelbeds.com/hotel-api/1.0/status'
-        xsignature = generateHash(ENV['API_KEY'] + ENV['SECRET'] + DateTime.now.strftime("%Q"))
-        puts xsignature
-        
-        HTTParty.get(
-            url,
-            headers: {
-                'api-key': ENV['API_KEY'],
-                'x-signature': xsignature,
-                "Content-Type": "application/json",
-            }
-        )
-      end
+        def self.by_location(location)
+            url = 'https://api.test.hotelbeds.com/hotel-api/1.0/status'
+            # url = 'https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels'
+            xsignature = (ENV['API_KEY'] + ENV['SECRET'] + DateTime.now.strftime("%Q")).hash
+            xsignature = xsignature.to_s
+            console.log(xsignature)
+            
+            @response = HTTParty.get(
+                url,
+                headers: {
+                    'api-key': ENV['API_KEY'],
+                    'x-signature': xsignature,
+                    "Content-Type": "application/json",
+                    'accept': 'application/json', 
+                    'accept-encoding': 'gzip'
+                }
+            )
+        end
 
-      def generateHash(str)
-        str.hash
-      end
+        def generateHash(str)
+            str.hash
+        end
+      
     end
-  end
+end
