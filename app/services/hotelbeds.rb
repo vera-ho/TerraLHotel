cdd671ea57781f3e010cfbe94dc9bba2a0356a4c
Vera Ho <vh71886@gmail.com>
2022-08-09 18:29:57 -0400

    Add hotelbeds code

--- app/services/hotelbeds.rb
+++ app/services/hotelbeds.rb
@@ -1,14 +1,23 @@
 module HotelBeds
     class Search
       def self.by_location(location)
-        # url = 'https://sandbox.impala.travel/v1/hotels'
+        url = 'https://api.test.hotelbeds.com/hotel-api/1.0/status'
+        puts "TIME *********** "
+        puts DateTime.now
+        xsignature = generateHash(ENV['API_KEY'] + ENV['SECRET'] + DateTime.now)
+        puts xsignature
         HTTParty.get(
-            # url,
+            url,
             headers: {
-                # 'x-api-key': ENV['API_KEY'],
+                'api-key': ENV['API_KEY'],
+                'x-signature': xsignature,
                 "Content-Type": "application/json",
             }
         )
+      end
+
+      def generateHash(str)
+        str.hash
       end
     end
   end
